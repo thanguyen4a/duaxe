46086136c18a62ca98d1508e2ac8f85d71353aef
NGUYEN TRI THA <00267-nguyen@NPC-201511-008.local>
2016-04-07 16:49:47 +0900

    add build command

---
+++ Assets/Editor/CommandLineBuild.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+
+
+public class CommandLineBuild
+{
+	static private string[] collectBuildScenes()
+	{
+		var scenes = new List<string>();
+
+		foreach (var scene in EditorBuildSettings.scenes)
+		{
+			if (scene == null)
+				continue;
+			if (scene.enabled)
+				scenes.Add(scene.path);
+		}
+		return scenes.ToArray();
+	}
+
+	[MenuItem(@"Build/BuildIOS")]
+	static public void buildIOS()
+	{
+		var scenes = collectBuildScenes();
+		BuildPipeline.BuildPlayer(scenes, "ios", BuildTarget.iOS, BuildOptions.None);
+	}
+}
